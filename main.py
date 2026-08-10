a3e7717de2969510864abc1fa55e488c2c11e812
HOT8OY <4321qwaszx@naver.com>
2026-08-10 16:56:47 +0900

    Refactoring 사용하지 않는 코드 삭제

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-from quiz_model import Quiz
-from quiz_view import QuizView
 from quiz_controller import QuizController
-
-def main():
-
-    model = QuizModel()
-
-    view = QuizView()
-
-    controller = QuizController(model, view)
-
-    controller.run()
 
 if __name__ == "__main__":
     app = QuizController()
